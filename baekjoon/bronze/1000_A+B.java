ab3aeeb69ccab9248387acfc00403b9f8e2aaac6
wkdwlgo <50902999+wkdwlgo@users.noreply.github.com>
2022-05-25 22:47:05 +0900

    Create 1000_A+B.java

---
+++ baekjoon/bronze/1000_A+B.java
@@ -0,0 +1,12 @@
+public class Main {
+	public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        int a,b,c;
+        system.out.println("1번");
+        a=in.nextInt();
+        system.out.println("1번");
+        b=in.nextInt();
+        c=a+b;
+		System.out.println(c);
+	}
+}
