00615d9151e75ea3ed1d55798102d50564895c39
jisoleil <50902999+wkdwlgo@users.noreply.github.com>
2022-12-09 21:19:50 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.10 ms, Memory: 76.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -0,0 +1,16 @@
+class Solution {
+   
+		 	public static long solution(int n) {
+		 	long memo[]= new long[2001];
+	        long answer = 0;
+	       memo[1]=1;
+	       memo[2]=2;
+	       for (int i = 3; i <= n; i++) {
+	              memo[i] = (memo[i - 1] + memo[i - 2]) % 1234567;
+	       }
+	        
+	       return memo[n];
+	    
+	    }
+    
+}
