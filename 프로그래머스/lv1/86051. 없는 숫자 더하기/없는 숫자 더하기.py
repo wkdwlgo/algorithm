07f3c3c982a18bc6e7dd2bfe21ca012822a1d2d7
jisoleil <50902999+wkdwlgo@users.noreply.github.com>
2023-02-03 15:54:20 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.py
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.py
@@ -1,11 +1,7 @@
 def solution(numbers):
     answer = 0
-    a=[0,1,2,3,4,5,6,7,8,9]
-    for i in range(0,len(numbers)):
-        for j in range(0, len(a)):
-            if(numbers[i]==a[j]):
-                   a[j]=0
-    for i in range(0, len(a)):
-        answer+=a[i]
+    for i in range(10):
+        if i not in numbers:
+            #print(i)
+            answer+=i
     return answer
-    
